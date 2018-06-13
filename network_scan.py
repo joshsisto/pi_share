a4fad10325ca9e8b1ef474564cbe85532f8b83c0
llamafarmer <joshsisto@gmail.com>
2018-06-12 21:58:13 -0700

    scanner

--- network_scan.py
+++ network_scan.py
@@ -1,32 +1,39 @@
 #!/usr/bin/env python
 import subprocess
 import os
+import socket
+
+
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
+print("Your Computer Name is: " + hostname)
+print("Your Computer IP Address is: " + IPAddr)
 
 # Do not display output
 FNULL = open(os.devnull, 'w')
 
 
 def pingNetwork():
-        network = input('Enter network i.e [192.168.2.0]: ')[0:-1]
-        starting_ip = input('Starting IP: ')
-        ending_ip = input('Ending IP: ')
+    network = IPAddr.split('.')
+    starting_ip = input('Starting IP: ')
+    ending_ip = input('Ending IP: ')
 
-        for i in range(int(starting_ip), int(ending_ip)):
+    for i in range(int(starting_ip), int(ending_ip)):
 
-            try:
-                subprocess.check_call(['ping', '-c', '1', network + str(i)])   #, stdout=FNULL,stderr=FNULL)
+        try:
+            subprocess.check_call(['ping', '-c', '1', f"{network[0]}.{network[1]}.{network[2]}." + str(i)], stdout=FNULL, stderr=FNULL)
 
-            except (OSError, subprocess.CalledProcessError):
+        except (OSError, subprocess.CalledProcessError):
 
-                print("[-] DOWN {}{}".format(network,i))
-            else:
+            print("[-] DOWN", f"{network[0]}.{network[1]}.{network[2]}." + str(i))
+        else:
 
-                print("[+] UP {}{}".format(network,i))
+            print("[+] UP", f"{network[0]}.{network[1]}.{network[2]}." + str(i))
 
 
 # pingNetwork()
 
-my_network = ["ASUNTU", "beeplus", "zero", "PiHAT2", "ASUS10"]
+my_network = ["ASUNTU", "beeplus", "zero", "PiHAT2", "ASUS10", "lego"]
 
 
 
@@ -34,7 +41,7 @@ def pingList():
     for device in my_network:
         # print(device + ".local")
         try:
-            subprocess.check_call(['ping', '-c', '1', device + '.local'], stdout=FNULL,stderr=FNULL)
+            subprocess.check_call(['ping', '-c', '1', device + '.local'], stdout=FNULL, stderr=FNULL)
 
         except (OSError, subprocess.CalledProcessError):
 
